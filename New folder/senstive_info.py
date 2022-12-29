2a2e71c4ba05f2323bc182c9f40eadd071bdeb83
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2022-12-29 17:43:00 -0500

    revamped organization
    same code but reorganized

---
+++ New folder/senstive_info.py
@@ -0,0 +1,12 @@
+creditcard_info = {'card number': 4506445737319085, 
+                   'card name': 'Mohamad Radaideh',
+                   'security number': 414,
+                   'exp. date': '0627'
+                   }
+
+account_info = {'email': 'mohparrot@gmail.com',
+                'secuirty code': ''
+                }
+
+iphone_account_info = {'Email': 'mohparrot@icloud.com',
+                       'Password': 'Oscardude1$'}
